2ada2dc8c5b4164cee81b4be160c4e04f3a744a9
Hunter Kassow <hunterkassow@protonmail.com>
2022-05-19 11:43:17 -0400

    commit tree 2739d1a21df97f9326c963c4df726897bb4e6b34

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main').remove()
+
+const newHeader = document.createElement("h1");
+newHeader.textContent = 'YOUR-NAME is the champion'
+newHeader.id = 'victory'
+document.body.append(newHeader);
